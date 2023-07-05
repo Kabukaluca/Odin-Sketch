84c605a14a1819c7b3e87c3f3e9612a3ed3914ff
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-07-05 06:13:29 +0200

    Create 'clear button' to clear painted grid

--- EaS.js
+++ EaS.js
@@ -1,6 +1,9 @@
 const sketchCont = document.getElementById("sketch-container");
 const input = document.querySelector("#slider");
 const gridSizeSpan = document.querySelector(".gridSize");
+const clear = document.querySelector("#clearBtn")
+let isDrawing = false;
+
 
 // create divs
 function createDivs() {
@@ -14,14 +17,42 @@ function createDivs() {
         let divRow = document.createElement("div");
         divRow.classList.add("divRow");
         divCol.appendChild(divRow);
+
+        divRow.addEventListener("click", () => {
+          divRow.style.backgroundColor = "black";
+        });
     }
   sketchCont.appendChild(divCol);
     }
-}
+};
+
+//Handle drawing event
+function handleDrawing (event) {
+  if (isDrawing) {
+    const divRow = event.target;
+    divRow.style.backgroundColor = "black";
+  };
+};
+
+
+//Eventlisteners for Drawing
+sketchCont.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+sketchCont.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+sketchCont.addEventListener("mousemove", handleDrawing);
+
 createDivs();
+
 
 // DOM functions
 input.addEventListener("input", () => {
-    gridSizeSpan.textContent = input.value;
+    gridSizeSpan.textContent = input.value + " x " + input.value;
 });
 input.addEventListener("input", createDivs);
+
+clear.addEventListener("click", createDivs);
